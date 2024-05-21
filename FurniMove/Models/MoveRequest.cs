06d752334c0b4ed3b5c76cfc3de61dee3b2971fa
mohamed20200469 <mohamedmagdyy01@hotmail.com>
2024-05-21 13:26:47 +0300

    minor adjustment

--- FurniMove/Models/MoveRequest.cs
+++ FurniMove/Models/MoveRequest.cs
@@ -15,21 +15,21 @@ namespace FurniMove.Models
         public string? serviceProviderId { get; set; }
         [Required]
         public required string status { get; set; }
-        public DateTime? startTime { get; set; }
-        public DateTime? endTime { get; set; }
-        public int? rating { get; set; }
-        public double? cost { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public int rating { get; set; }
+        public double cost { get; set; }
         public int numOfAppliances { get; set; }
         public ICollection<Appliance> appliances { get; } = new List<Appliance>();
         public Location? startLocation { get; set; }
         public Location? endLocation { get; set;}
         [ForeignKey("startLocation")]
-        public int? startLocationId { get; set; }
+        public int startLocationId { get; set; }
         [ForeignKey("endLocation")]
-        public int? endLocationId { get; set; }
+        public int endLocationId { get; set; }
         public Truck? truck { get; set; }
         [ForeignKey("truck")]
-        public int? truckId { get;set; }
+        public int truckId { get;set; }
 
     }
 }
